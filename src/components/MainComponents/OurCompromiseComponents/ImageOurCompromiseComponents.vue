edef4dbdacfe5d54dd1c2af6ea8e71f443ef011e
Pestana Pedro Catumbela <ti09@pacaembuautopecas.com.br>
2023-08-15 11:55:15 -0300

    style: Estilizando Our Compromise

--- src/components/MainComponents/OurCompromiseComponents/ImageOurCompromiseComponents.vue
+++ src/components/MainComponents/OurCompromiseComponents/ImageOurCompromiseComponents.vue
@@ -12,11 +12,13 @@
 
 <style scoped>
     .compromise__image{
-        top: 200px;
-        left: 62px;
+        /* top: 1480px; */
+        /* left: 696px; */
+        top: 210%;
+        left: 45.8%;
         width: 1200px;
-        height: 500px;
+        height: 600px;
 
-        position: relative;
+        position: absolute;
     }
 </style>
